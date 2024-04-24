c4772c97a1603e2d1c4a40ae9d7627819684cd1f
Vyacheslav1557 <slavabirin1@yandex.ru>
2024-04-24 19:30:53 +0500

    fix: add lock

--- internal/lib/id.go
+++ internal/lib/id.go
@@ -1,18 +1,24 @@
 package lib
 
+import "sync"
+
 type IdentifierManager struct {
 	lastInsertedID int64
+	mtx            sync.Mutex
 }
 
 func NewIdentifierManager() *IdentifierManager {
 	return &IdentifierManager{
 		lastInsertedID: 10000,
+		mtx:            sync.Mutex{},
 	}
 }
 
 func (m *IdentifierManager) NewID() int64 {
+	m.mtx.Lock()
 	defer func() {
 		m.lastInsertedID++
+		m.mtx.Unlock()
 	}()
 	return m.lastInsertedID
 }
